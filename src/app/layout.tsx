d13c8404e14300aec63f0f47ae303a9feb83b7ae
CIVITERA <info@civiteragov.com>
2026-05-14 20:34:54 +0800

    Add page transition animation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,11 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        <div className="animate-page-fade">
+          {children}
+        </div>
+      </body>
     </html>
   );
 }
